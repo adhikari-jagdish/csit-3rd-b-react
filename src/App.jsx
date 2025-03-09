737ce0de67febb9e33b444a1bad0e924f8ea5614
Jagadish <adhikari.jagdish5200@gmail.com>
2025-03-09 10:04:07 +0545

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/home';
+import Movies from './components/movies';
+
+
+const App = () => {
+
+
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+        </Routes>
+    );
+};
+
+export default App
---
+++ src/components/home.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <>
+            <h1>Home</h1>
+            <Link to="/movies">
+                <button>View Movies</button>
+            </Link>
+
+
+        </>
+    )
+}
+
+export default Home
